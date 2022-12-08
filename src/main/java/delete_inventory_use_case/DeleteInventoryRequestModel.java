63093202bf942e91a0b6f39f1fa545026a15d107
DaneshKohina <danesh.kohina@mail.utoronto.ca>
2022-12-07 23:36:15 -0500

    These are my final files for the Analysis feature

--- src/main/java/delete_inventory_use_case/DeleteInventoryRequestModel.java
+++
@@ -1,9 +0,0 @@
-package delete_inventory_use_case;
-
-public class DeleteInventoryRequestModel {
-    String invName;
-
-    public DeleteInventoryRequestModel(String name) {
-        this.invName = name;
-    }
-}
