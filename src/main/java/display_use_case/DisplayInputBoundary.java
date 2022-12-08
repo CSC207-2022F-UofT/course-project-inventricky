cbc164c1040d63b5b2fc8b2e22cffdbd3d920a67
guzzim2022 <m.g1@mail.com>
2022-12-08 11:10:40 -0500

    Only exporting

--- src/main/java/display_use_case/DisplayInputBoundary.java
+++
@@ -1,7 +0,0 @@
-package display_use_case;
-import Screens.InventoryViewModel;
-
-public interface DisplayInputBoundary {
-
-    InventoryViewModel create(DisplayRequestModel displayRequestModel);
-}
