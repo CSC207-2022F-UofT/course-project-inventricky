f9346ed03f7438545e0c513e8149c6f5635003c9
chenbriann <briann.chen@mail.utoronto.ca>
2022-12-08 01:06:25 -0500

    Prepare code for pull request.

--- src/main/java/delete_inventory_use_case/DeleteInventoryInputBoundary.java
+++
@@ -1,8 +0,0 @@
-package delete_inventory_use_case;
-
-import Screens.InventoryViewModel;
-
-public interface DeleteInventoryInputBoundary {
-
-    InventoryViewModel deleteInventory(DeleteInventoryRequestModel requestModel, boolean testing);
-}
